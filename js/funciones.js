90a54f960a16141cb4c540344ba62d50f940ab4c
NickB2 <51224240+NickB2@users.noreply.github.com>
2019-12-10 16:20:08 -0300

    Update 3

--- js/funciones.js
+++ js/funciones.js
@@ -1,48 +1,53 @@
 String.prototype.trim = function () {
     return this.replace(/^\s*/, "").replace(/\s*$/, "");
 }
-  
-function validarEstilo(id){
-    var campo = "#"+id
+
+function scrollear(id) {
+    document.getElementById(id).scrollIntoView()
+}
+
+
+function validarEstilo(id) {
+    var campo = "#" + id
     var valor = $(campo).val().trim();
-    if(valor == ''){
+    if (valor == '') {
         $(campo).removeClass('is-valid').addClass('is-invalid')
     } else {
-        if (valor == '0'){
-        $(campo).removeClass('is-valid').addClass('is-invalid')
+        if (valor == '0') {
+            $(campo).removeClass('is-valid').addClass('is-invalid')
         } else {
-        $(campo).removeClass('is-invalid').addClass('is-valid')
+            $(campo).removeClass('is-invalid').addClass('is-valid')
         }
     }
 }
 
-function desvalidarEstilo(id){
-    var campo = "#"+id
+function desvalidarEstilo(id) {
+    var campo = "#" + id
     $(campo).removeClass('is-invalid').removeClass('is-valid')
 }
 
-function validarEstiloTelefono(id){
-    var campo = "#"+id
+function validarEstiloTelefono(id) {
+    var campo = "#" + id
     var valor = $(campo).val().trim();
-    if(valor == ''){
-        $(campo).removeClass('is-valid')
+    if (valor == '') {
+        $(campo).removeClass('is-valid').removeClass('is-invalid');
     } else {
-        if(!isNaN(valor)){
-            $(campo).removeClass('is-invalid').addClass('is-valid')    
+        if (!isNaN(valor)) {
+            $(campo).removeClass('is-invalid').addClass('is-valid')
         } else {
             $(campo).removeClass('is-valid').addClass('is-invalid')
         }
-        
+
     }
 }
 
-function obtenerValor(id){
-    var campo = "#"+id
+function obtenerValor(id) {
+    var campo = "#" + id
     var valor = $(campo).val().trim()
     return valor
 }
 
-function validar(){
+function validar() {
     var nombre = obtenerValor('id_inputNombre');
     var apellido = obtenerValor('id_inputApellido');
     var telefono = obtenerValor('id_inputTelefono');
@@ -57,7 +62,7 @@ function validar(){
     validarEstilo('id_inputAsunto');
     validarEstilo('id_inputMensaje');
 
-    if(nombre && apellido && email && asunto && mensaje){
+    if (nombre && apellido && email && asunto && mensaje) {
         var estadoTelefono = 1
         console.log('Todo bien')
         datos = {
@@ -68,19 +73,19 @@ function validar(){
             'mensaje': mensaje
         }
 
-        if (telefono != ''){
-            if(!isNaN(telefono)){
-                Object.assign(datos,{'telefono': telefono})
-            } else  {
+        if (telefono != '') {
+            if (!isNaN(telefono)) {
+                Object.assign(datos, { 'telefono': telefono })
+            } else {
                 estadoTelefono = 0
             }
         }
-        if(estadoTelefono == 1){
+        if (estadoTelefono == 1) {
             Swal.fire({
                 icon: 'success',
-                text:   'Tus dudas y consultas son importantes para nostros, '+
-                        'nos pondremos en contacto contigo en cuanto podamos'
-            }).then(function(){
+                text: 'Tus dudas y consultas son importantes para nostros, ' +
+                    'nos pondremos en contacto contigo en cuanto podamos'
+            }).then(function () {
                 $('#id_formConsulta').trigger('reset')
                 desvalidarEstilo('id_inputNombre');
                 desvalidarEstilo('id_inputApellido');
